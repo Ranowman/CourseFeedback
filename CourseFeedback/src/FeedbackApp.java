cc6679169af71dcf0f4430505f008d152f87f0d1
Ranowman <59177824+Ranowman@users.noreply.github.com>
2020-01-15 21:03:53 -0600

    layered panel for class option menu

--- CourseFeedback/src/FeedbackApp.java
+++ CourseFeedback/src/FeedbackApp.java
@@ -9,10 +9,13 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JLayeredPane;
+import java.awt.CardLayout;
 
 public class FeedbackApp {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -51,36 +54,70 @@ public class FeedbackApp {
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(566, 320, 357, 371);
+		panel.add(layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+		layeredPane.setVisible(false);
+		
 		JPanel panel_1 = new JPanel();
+		layeredPane.add(panel_1, "name_29355439673600");
 		panel_1.setBackground(Color.PINK);
-		panel_1.setBounds(150, 338, 368, 327);
-		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnCs = new JButton("CS250");
+		btnCs.setBounds(66, 71, 197, 47);
+		panel_1.add(btnCs);
 		panel_1.setVisible(false);
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(Color.PINK);
-		panel_2.setBounds(549, 348, 386, 317);
-		panel.add(panel_2);
+		layeredPane.add(panel_2, "name_29131305335800");
+		panel_2.setBackground(Color.ORANGE);
+		panel_2.setLayout(null);
 		
-		JButton btnButton = new JButton("button1");
+		JButton btnCs_1 = new JButton("CS250");
+		btnCs_1.setBounds(76, 85, 197, 47);
+		panel_2.add(btnCs_1);
+		panel_2.setVisible(false);
+		
+		JButton btnButton = new JButton("Add Tips");
 		btnButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				layeredPane.setVisible(true);
 				panel_1.setVisible(true);
 				panel_2.setVisible(false);
 			}
 		});
-		btnButton.setBounds(220, 191, 197, 47);
+		btnButton.setBounds(635, 190, 197, 47);
 		panel.add(btnButton);
 		
-		JButton btnButton_1 = new JButton("button2");
+		JButton btnButton_1 = new JButton("View Tips");
 		btnButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				layeredPane.setVisible(true);
 				panel_1.setVisible(false);
 				panel_2.setVisible(true);
 			}
 		});
-		btnButton_1.setBounds(610, 191, 197, 47);
+		btnButton_1.setBounds(635, 260, 197, 47);
 		panel.add(btnButton_1);
-		panel_2.setVisible(false);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setBounds(31, 135, 479, 589);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		
+		
+		
+		
+		
+		
+
+		
+	
+		
+		
 	}
 }
